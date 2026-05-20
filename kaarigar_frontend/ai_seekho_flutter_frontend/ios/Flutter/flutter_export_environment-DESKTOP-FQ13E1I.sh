fab7184fd91b564c6278f6172aa66a4bfbf8aa34
Laibakamalkhan <152787902+Laibakamalkhan@users.noreply.github.com>
2026-05-21 02:35:44 +0500

    New PR

--- kaarigar_frontend/ai_seekho_flutter_frontend/ios/Flutter/flutter_export_environment-DESKTOP-FQ13E1I.sh
+++ kaarigar_frontend/ai_seekho_flutter_frontend/ios/Flutter/flutter_export_environment-DESKTOP-FQ13E1I.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\MY PC\Desktop\DSU-(23-27)\Projects\ai seekho hack fe\ai_seekho_flutter_frontend"
+export "FLUTTER_ROOT=C:\Users\Irtiza\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Irtiza\OneDrive\Desktop\APP test\kaarigar_frontend\ai_seekho_flutter_frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
